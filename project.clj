9d68f431732dc797fd4e6ac2e3fd9f741bd730f5
Curtis Wolterding <curtiswolterding@gmail.com>
2012-08-05 19:00:28 -0400

    upgraded to lein version 2. changed project to match the output of lein1 precate.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject kindlemail "1.0.0-SNAPSHOT"
-  :description "A simple utility for emailing files/webpages to a Kindle device."
-  :author "Curtis Wolterding"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.draines/postal "1.8.0"]
                  [org.clojure/tools.cli "0.2.1"]]
-  :main kindlemail.core)
+  :author "Curtis Wolterding"
+  :main kindlemail.core
+  :min-lein-version "2.0.0"
+  :description "A simple utility for emailing files/webpages to a Kindle device.")
 
